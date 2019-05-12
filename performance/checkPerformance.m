f6815b33bee3e3f58ffc92869c9710cbf2af1528
adnansufian <a.sufian@imperial.ac.uk>
2019-05-12 12:41:34 +0100

    Updated performance check scripts and added figure for readme file.

--- performance/checkPerformance.m
+++ performance/checkPerformance.m
@@ -1,41 +1,64 @@
 function [] = checkPerformance()
-	% CHECKPERFORMANCE measures the improved speed up from using 'lammpsReader' as
-	% opposed to using existing matlab functionality.
+% CHECKPERFORMANCE measures the improved speed up from using 'lammpsReader' as
+% opposed to using existing matlab functionality (e.g importdata).
 
-	% The methods compared include:
-	% (i) lammpsReader
-	% (ii) importdata
-
+	% Add lammpsReader to path
 	addpath('../')
 
+	% Set the number of header rows (required for importdata) and the number of
+	% data rows and columns (which should be the same as the makeDumpFiles.m
+	% script). Set the number of times each case is repeated.
 	numHeaderRows = 9;
+	numCol = 10;
 	numRow = logspace(0, 7, 8);
-	time = zeros(length(numRow), 2);
+	numRepeat = 3;
+
+	% Using tic-toc command, extract the time taken to read the dump file using
+	% both importdata and lammpsReader.
+	time_importdata = zeros(length(numRow), numRepeat);
+	time_lammpsReader = zeros(length(numRow), numRepeat);
 	for i = 1:length(numRow)
 		filename = sprintf('file_%d.dump', numRow(i));
 
-		tic
-		file1 = importdata(filename, ' ', numHeaderRows);
-		time(i, 1) = toc;
+		for j = 1:numRepeat
+			tic
+			file1 = importdata(filename, ' ', numHeaderRows);
+			time_importdata(i, j) = toc;
+		end
 
-		tic
-		file2 = lammpsReader(filename);
-		time(i, 2) = toc;
+		for j = 1:numRepeat
+			tic
+			file2 = lammpsReader(filename);
+			time_lammpsReader(i, j) = toc;
+		end
 
+		% Check that the data matches between the two methods.
 		assert(all(all(abs(file1.data - file2.data)<1e-6)))
 
-		fprintf('%d rows: importdata ran in %g sec & lammpsReader ran in %g sec\n', numRow(i), time(i, 1), time(i, 2))
+		% Display the average time required in each case.
+		fprintf('%d rows: importdata ran in %g sec & lammpsReader ran in %g sec\n', ...
+			numRow(i), mean(time_importdata(i, :)), mean(time_lammpsReader(i, :)))
 	end
 
+	% Generate figure
 	figure()
 	hold on
-	plot(numRow, time(:, 1), 'bs-')
-	plot(numRow, time(:, 2), 'ro-')
-	xlabel('Number of Rows')
-	ylabel('Total time')
+	errorbar(numRow*numCol, ...
+		mean(time_importdata, 2), ...
+		mean(time_importdata, 2) - min(time_importdata, [], 2), ...
+		max(time_importdata, [], 2) - mean(time_importdata, 2), ...
+		'LineWidth', 1.5)
+	errorbar(numRow*numCol, ...
+		mean(time_lammpsReader, 2), ...
+		mean(time_lammpsReader, 2) - min(time_lammpsReader, [], 2), ...
+		max(time_lammpsReader, [], 2) - mean(time_lammpsReader, 2), ...
+		'LineWidth', 1.5)
+	xlabel('Number of Elements')
+	ylabel('Total time (s)')
 	set(gca, 'XScale', 'log')
-	legend({'importdata', 'lammpsReader'}, 'Location', 'NorthWest')
-	print -dpng -r100 -f1 checkPerformance
+	set(gca, 'YScale', 'log')
+	legend({'importdata', 'lammpsReader'}, 'Location', 'SouthEast')
+	print -dpng -r150 -f1 checkPerformance
 	close all
 end
 
--- performance/makeDumpFiles.m
+++ performance/makeDumpFiles.m
@@ -1,12 +1,16 @@
 function [] = makeDumpFiles()
 % MAKEDUMPFILES generates mock dump files to test the performance of
 % LAMMPSREADER compared to existing options in matlab and reading in line by
-% line. A total of 10 dump files are created with each row containing 10
-% entries, and there being 1e0, 1e1, 1e2, ..., 1e9 rows.
+% line. A total of 8 dump files are created with each row containing 10
+% entries, and there being 1e0, 1e1, 1e2, ..., 1e7 rows.
 	
 	numCol = 10;
 	numRow = logspace(0, 7, 8);
+	assert(mod(numCol,2)==0)
 	for i = 1:length(numRow)
+		fprintf('Creating file: file_%d.dump\n', numRow(i))
+		
+		% Create the header information.
 		fid = fopen(sprintf('file_%d.dump', numRow(i)), 'w+');
 		fprintf(fid, 'ITEM: TIMESTEP\n');
 		fprintf(fid, '7000000\n');
@@ -17,7 +21,12 @@ function [] = makeDumpFiles()
 		fprintf(fid, '0 0.0131534\n');
 		fprintf(fid, '0 0.0131559\n');
 		fprintf(fid, 'ITEM: ENTRIES c_pair[1] c_pair[2] c_pair[3] c_pair[4] c_pair[5] c_pair[6] c_pair[7] c_pair[8] c_pair[9] c_pair[10]\n');
-		fprintf(fid, '%f %f %f %f %f %f %f %f %f %f\n', rand(numRow(i), numCol));
+		
+		% Create the data array. This includes random floats and integers mimicking
+		% actual dump output.
+		data = (rand(numRow(i)*numCol, 1).^5).*rand(numRow(i)*numCol, 1);
+		data(randi(length(data), floor(0.2*length(data)), 1)) = randi(length(data), floor(0.2*length(data)), 1);
+		fprintf(fid, '%g %g %g %g %g %g %g %g %g %g\n', data);
 		fclose(fid);
 	end
 end
